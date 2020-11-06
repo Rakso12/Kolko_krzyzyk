5ad6397d2b10b01b9c08d53a0cf139d181f77473
Oskar Tyniec <39619380+Rakso12@users.noreply.github.com>
2020-11-06 21:17:29 +0100

    Podstawy
    Ogarnięcie sobie podstaw sfml

--- Kolko_krzyzyk_project/Kolko_krzyzyk_project/main.cpp
+++ Kolko_krzyzyk_project/Kolko_krzyzyk_project/main.cpp
@@ -1,24 +1,30 @@
 //ten super main którego musze zapisaæ
 #include <SFML/Graphics.hpp>
 
+
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(400, 400), "SFML works!");
-    sf::CircleShape shape(100.0f);
-    shape.setFillColor(sf::Color::Red);
+    sf::RenderWindow window(sf::VideoMode(400, 400), "Taka fajna dupa");
+    sf::RectangleShape kwadrat;
+
+    kwadrat.setSize(sf::Vector2f(100,100));
+    kwadrat.setFillColor(sf::Color::Blue);
+    kwadrat.setPosition(sf::Vector2f(200,200));
+    kwadrat.setOrigin(kwadrat.getSize().x/2,kwadrat.getSize().y/2);
 
     while (window.isOpen())
     {
-        sf::Event event;
+        sf::Event event; // klasa zdarzenia :) zdarzenia z okna odczytaj¹ sie w event 
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
 
-        window.clear();
-        window.draw(shape);
+        window.clear();  
+        window.draw(kwadrat);
         window.display();
+
     }
 
     return 0;
