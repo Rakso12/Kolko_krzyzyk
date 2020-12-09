ae146594056fb1c62c6a35f0ecbd767479e6f88d
Oskar Tyniec <39619380+Rakso12@users.noreply.github.com>
2020-12-09 22:47:18 +0100

    Dodanie sprawdzania pion/poziom
    Do zrobienia przekątne

--- Kolko_krzyzyk_project/Kolko_krzyzyk_project/Check.cpp
+++ Kolko_krzyzyk_project/Kolko_krzyzyk_project/Check.cpp
@@ -3,19 +3,73 @@
 
 int Check::czyWygrana(std::vector<std::vector<Field*>> pola, int rozmiar)
 {
-	int licznik_y = 0, licznik_x = 0;
-	for (int i = 0; i < rozmiar; i++)
-	{
-		for (int j = 0; j < rozmiar; j++)
+	// sprawdzanie pionu
+
+	int licznik = 0;
+	int ile_wygrywa = 3;
+	bool tmp = false;
+
+	// sprawdzanie pionu
+	
+	if (!tmp) {
+		for (int i = 0; i < rozmiar; i++)
 		{
-			if( pola[j][i]->getText().getString().toAnsiString() == "X") // Do poprawy warunek z sf::Text na chara czy coœ
-			{
-				licznik_y++;
-			}
-			else {
-				licznik_y = 0;
+			for (int j = 0; j < rozmiar; j++) {
+				if (pola[j][i]->getText().getString().toAnsiString() == "X")
+				{
+					licznik++;
+					if (licznik == ile_wygrywa) {
+						tmp = true;
+					}
+				}
+				else {
+					licznik = 0;
+				}
 			}
 		}
 	}
+
+	// sprawdzanie poziomu
+
+	if (!tmp) {
+		for (int i = 0; i < rozmiar; i++)
+		{
+			for (int j = 0; j < rozmiar; j++) {
+				if (pola[i][j]->getText().getString().toAnsiString() == "X")
+				{
+					licznik++;
+					if (licznik == ile_wygrywa) {
+						tmp = true;
+					}
+				}
+				else {
+					licznik = 0;
+				}
+			}
+		}
+	}
+	
+	// sprawdzanie skosu od lewej
+
+	// DO ZROBIENIA SPRAWDZANIE PRZEK¥TNYCH POD I NAD / LEWA / PRAWA
+	/*
+	if (!tmp) {
+		for (int i = 0; i < rozmiar-1; i++)
+		{
+			if (pola[i+1][i]->getText().getString().toAnsiString() == "X")
+			{
+				licznik++;
+				if (licznik == ile_wygrywa) {
+					tmp = true;
+				}
+			}
+			else {
+				licznik = 0;
+			}
+		}
+	}
+	*/
+	std::cout << tmp << "wynik" << std::endl;
+
 	return 0;
 }
