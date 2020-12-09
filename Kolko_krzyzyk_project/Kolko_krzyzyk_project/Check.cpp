072c187eada26eea287577e21836a429f2f1a7c4
Oskar Tyniec <39619380+Rakso12@users.noreply.github.com>
2020-12-09 16:46:17 +0100

    próba rozgryzienia warunku
    do ogarnięcia jak ten warunek ma działać

--- Kolko_krzyzyk_project/Kolko_krzyzyk_project/Check.cpp
+++ Kolko_krzyzyk_project/Kolko_krzyzyk_project/Check.cpp
@@ -3,13 +3,17 @@
 
 int Check::czyWygrana(std::vector<std::vector<Field*>> pola, int rozmiar)
 {
+	int licznik_y = 0, licznik_x = 0;
 	for (int i = 0; i < rozmiar; i++)
 	{
 		for (int j = 0; j < rozmiar; j++)
 		{
 			if( pola[j][i]->getText().getString().toAnsiString() == "X") // Do poprawy warunek z sf::Text na chara czy coœ
 			{
-				
+				licznik_y++;
+			}
+			else {
+				licznik_y = 0;
 			}
 		}
 	}
